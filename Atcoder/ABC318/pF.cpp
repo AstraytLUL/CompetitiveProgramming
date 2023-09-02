80a13c3b770586d596c819ae1ebd303f4601dfde
AstraytLUL <terrisgd@gmail.com>
2023-09-03 03:41:26 +0800

    Updated ABC318 F Solution (WA)

--- Atcoder/ABC318/pF.cpp
+++ Atcoder/ABC318/pF.cpp
@@ -38,13 +38,18 @@ void solve(){
     p.pb(-1000000000000000000), p.pb(1000000000000000001);
     for(int i = 0; i < n; ++i) cin >> X[i];
     for(int i = 0; i < n; ++i) cin >> L[i];
+    set<int> sp;
     for(int i = 0; i < n; ++i){
         p.pb(X[i]);
         for(int j = i + 1; j < n; ++j){
             int tmp = X[i] + X[j];
             if(tmp % 2){
+                p.pb(tmp / 2);
                 p.pb(tmp / 2 + 1);
             }else{
+                sp.insert(tmp / 2);
+                p.pb(tmp / 2 - 1);
+                p.pb(tmp / 2);
                 p.pb(tmp / 2 + 1);
             }
         }
@@ -53,20 +58,34 @@ void solve(){
     p.resize(unique(p.begin(), p.end()) - p.begin());
     for(int i = 0; i < (int)p.size() - 1; ++i){
         int l = p[i], r = p[i + 1] - 1;
+        if(sp.find(l) != sp.end()) continue;
         vector<pii> dist;
         for(int j = 0; j < n; ++j){
             dist.pb(pii(abs(l - X[j]), j));
         }
         sort(dist.begin(), dist.end());
         for(int j = 0; j < n; ++j){
-            if(X[dist[j].ss] <= r){
+            if(X[dist[j].ss] <= l){
                 r = min(r, X[dist[j].ss] + L[j]);
             }
-            else if(X[dist[j].ss] >= l){
+            else if(X[dist[j].ss] >= r){
                 l = max(l, X[dist[j].ss] - L[j]);
             }
         }
         ans += max(0ll, r - l + 1);
+    }
+    for(int t:sp){
+        vector<pii> dist;
+        int ok = 1;
+        for(int j = 0; j < n; ++j){
+            dist.pb(pii(abs(t - X[j]), j));
+        }
+        sort(dist.begin(), dist.end());
+        for(int j = 0; j < n; ++j){
+            if(X[dist[j].ss] < t && t - L[j] > X[dist[j].ss]) ok = 0;
+            if(X[dist[j].ss] > t && t + L[j] < X[dist[j].ss]) ok = 0;
+        }
+        ans += ok;
     }
     cout << ans << '\n';
 }
