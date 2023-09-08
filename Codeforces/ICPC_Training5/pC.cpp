72004a977b395b7923edff9fca940709cda1824a
AstraytLUL <terrisgd@gmail.com>
2023-09-08 13:24:00 +0800

    Updated pC Solution

--- Codeforces/ICPC_Training5/pC.cpp
+++ Codeforces/ICPC_Training5/pC.cpp
@@ -21,7 +21,7 @@
 #include <fstream>
 using namespace std;
 typedef long long ll;
-#define int ll
+//#define int ll
 #define starburst ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define pii pair<int,int>
 #define pb push_back
@@ -31,39 +31,36 @@ typedef long long ll;
 #define maxn 200005
 #define mod 1000000007
 
-int val[205][205], dp[205][205];
-pair<int, pii> dp1[205][205];
+int val[305][305], dp[605][305][305];
 
 void solve(){
     int n; cin >> n;
     for(int i = 1; i <= n; ++i){
         for(int j = 1; j <= n; ++j){
             cin >> val[i][j];
-            dp1[i][j].ff = val[i][j];
         }
     }
-    for(int i = 1; i <= n; ++i){
-        for(int j = 1; j <= n; ++j){
-            if(i != 1){
-                if(dp1[i][j].ff < dp1[i - 1][j].ff + val[i][j]) dp1[i][j] = mp(dp1[i - 1][j].ff + val[i][j], mp(i - 1, j));
-            }
-            if(j != 1){
-                if(dp1[i][j].ff < dp1[i][j - 1].ff + val[i][j]) dp1[i][j] = mp(dp1[i][j - 1].ff + val[i][j], mp(i, j - 1));
+    for(auto &a:dp){
+        for(auto &b:a){
+            for(auto &c:b){
+                c = -598000;
             }
         }
     }
-    val[1][1] = 0;
-    pii cur(n, n);
-    while(cur != pii(1, 1)){
-        val[cur.ff][cur.ss] = 0;
-        cur = dp1[cur.ff][cur.ss].ss;
-    }
-    for(int i = n; i >= 1; i--){
-        for(int j = n; j >= 1; j--){
-            dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]) + val[i][j];
+    dp[2][1][1] = val[1][1];
+    for(int s = 3; s <= 2 * n; ++s){
+        for(int i = min(1, s - n); i <= min(s - 1, n); ++i){
+            for(int j = min(1, s - n); j <= min(s - 1, n); ++j){
+                int xi = i, yi = s - i;
+                int xj = j, yj = s - j;
+                int point = 0;
+                if(xi == xj && yi == yj) point = val[xi][yi];
+                else point = val[xi][yi] + val[xj][yj];
+                dp[s][i][j] = max({dp[s - 1][i][j], dp[s - 1][i - 1][j], dp[s - 1][i][j - 1], dp[s - 1][i - 1][j - 1]}) + point;
+            }
         }
     }
-    cout << dp[1][1] + dp1[n][n].ff << '\n';
+    cout << dp[2 * n][n][n] << '\n';
 }
 
 signed main(){
